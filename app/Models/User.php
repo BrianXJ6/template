d2d98ff81c39ff4938ade4c3b16064e2cc5b5e7b
Brian Barros <brianferreira15@hotmail.com>
2024-09-28 11:23:03 -0300

    Installing and Customizing Jetstream

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,22 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array<int,string>
      */
     protected $fillable = [
         'name',
@@ -25,17 +28,16 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array<int,string>
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
      * Get the attributes that should be cast.
      *
-     * @return array<string, string>
+     * @return array<string,string>
      */
     protected function casts(): array
     {
---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import { Head, Link, router } from "@inertiajs/vue3";
+
+defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div>
+        <Head :title="title" />
+        <h1>{{ title }}</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sed
+            natus quisquam dolores adipisci temporibus vel ducimus, quidem
+            impedit aut atque quos aliquid nostrum corrupti nisi consectetur
+            asperiores? Perferendis, aut?
+        </p>
+    </div>
+</template>
